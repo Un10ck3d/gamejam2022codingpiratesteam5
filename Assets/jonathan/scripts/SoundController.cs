76f3889764a67e5768dd50f26d26685f7af77d3f
Jonathan <jonathan@bangert.dk>
2022-11-13 00:04:24 +0100

    Credits

--- Assets/jonathan/scripts/SoundController.cs
+++ Assets/jonathan/scripts/SoundController.cs
@@ -10,7 +10,9 @@ public class SoundController : MonoBehaviour
     public AudioClip main_theme;
     // Start is called before the first frame update
     void Start()
-    {}
+    {
+        
+    }
 
     // Update is called once per frame
     void Update()
