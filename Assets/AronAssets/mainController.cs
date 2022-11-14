dfaa3006bcc60128d59cffbb73886616852b2fcb
Elliott <dbtracker.dk@gmail.com>
2022-11-14 20:07:44 +0100

    working mobile rot but broke keys

--- Assets/AronAssets/mainController.cs
+++ Assets/AronAssets/mainController.cs
@@ -1,6 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
+using UnityEngine.InputSystem;
 
 public class mainController : MonoBehaviour
 {
@@ -28,6 +31,10 @@ public class mainController : MonoBehaviour
 
     public IngameMenuScript menuScript;
 
+    private float SAngle;
+
+    private bool isMobile;
+
     void Awake()
     {
         Physics.gravity = new Vector3(0,-9.81f,0);
@@ -35,11 +42,29 @@ public class mainController : MonoBehaviour
     void Start()
     {
         rot = transform.eulerAngles.z;
+
+        if(AttitudeSensor.current != null){
+            InputSystem.EnableDevice(AttitudeSensor.current);
+
+        }
+
+        isMobile = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        isMobile = AttitudeSensor.current.attitude.ReadValue().eulerAngles.z != 0;
+        if(AttitudeSensor.current != null){
+
+            Debug.Log(AttitudeSensor.current.attitude.ReadValue().eulerAngles);
+            menuScript.level((int) AttitudeSensor.current.attitude.ReadValue().eulerAngles.z);
+            SAngle = AttitudeSensor.current.attitude.ReadValue().eulerAngles.z;
+        }
+        else{
+            Debug.Log("no censor");
+            menuScript.level(-10);
+        }
         //Debug.Log(curCameraPosition);
         for(int i = 0;i<=levels_forDisabling.Length-1;i++)
         {
@@ -50,7 +75,7 @@ public class mainController : MonoBehaviour
             else
             {
                 levels_forDisabling[i].SetActive(true);
-                menuScript.level(i+1);
+                // menuScript.level(i+1);
             }
         }
         if(cameraPositions.Length != 0)
@@ -63,20 +88,41 @@ public class mainController : MonoBehaviour
         }
         if(IsHitingWall)
         {
-            if(Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W)) {
-                rot += 180;
-                iTween.RotateTo(gameObject, iTween.Hash("rotation", new Vector3(0, 270, rot), "time", time, "easetype", iTween.EaseType.easeInOutCubic));
+            if(!isMobile){
             }
-            if(Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A)) {
-                rot -= 90;
-                iTween.RotateTo(gameObject, iTween.Hash("rotation", new Vector3(0, 270, rot), "time", time, "easetype", iTween.EaseType.easeInOutCubic));
+                if(Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W)) {
+                    rot += 180;
+                    iTween.RotateTo(gameObject, iTween.Hash("rotation", new Vector3(0, 270, rot), "time", time, "easetype", iTween.EaseType.easeInOutCubic));
+                }
+                if(Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A)) {
+                    rot -= 90;
+                    iTween.RotateTo(gameObject, iTween.Hash("rotation", new Vector3(0, 270, rot), "time", time, "easetype", iTween.EaseType.easeInOutCubic));
+                }
+                // if(Input.GetKeyDown(KeyCode.DownArrow)) {
+                //     iTween.RotateTo(gameObject, iTween.Hash("rotation", new Vector3(0, 270, transform.eulerAngles.z+rotOffset+0), "time", time, "easetype", iTween.EaseType.easeInOutCubic));
+                // }
+                if(Input.GetKeyDown(KeyCode.RightArrow)  || Input.GetKeyDown(KeyCode.D)) {
+                    rot += 90;
+                    iTween.RotateTo(gameObject, iTween.Hash("rotation", new Vector3(0, 270, rot), "time", time, "easetype", iTween.EaseType.easeInOutCubic));
             }
-            // if(Input.GetKeyDown(KeyCode.DownArrow)) {
-            //     iTween.RotateTo(gameObject, iTween.Hash("rotation", new Vector3(0, 270, transform.eulerAngles.z+rotOffset+0), "time", time, "easetype", iTween.EaseType.easeInOutCubic));
-            // }
-            if(Input.GetKeyDown(KeyCode.RightArrow)  || Input.GetKeyDown(KeyCode.D)) {
-                rot += 90;
-                iTween.RotateTo(gameObject, iTween.Hash("rotation", new Vector3(0, 270, rot), "time", time, "easetype", iTween.EaseType.easeInOutCubic));
+            else {
+
+                if(shouldMobileRot(90)){
+                    rot = 180;
+                    // iTween.RotateTo(gameObject, iTween.Hash("rotation", new Vector3(0, 270, rot), "time", time, "easetype", iTween.EaseType.easeInOutCubic));
+                }
+                if(shouldMobileRot(180)){
+                    rot = 90;
+                    // iTween.RotateTo(gameObject, iTween.Hash("rotation", new Vector3(0, 270, rot), "time", time, "easetype", iTween.EaseType.easeInOutCubic));
+                }
+                if(shouldMobileRot(-90)){
+                    rot = 0;
+                    // iTween.RotateTo(gameObject, iTween.Hash("rotation", new Vector3(0, 270, rot), "time", time, "easetype", iTween.EaseType.easeInOutCubic));
+                }
+                if(shouldMobileRot(0)){
+                    rot = -90;
+                    // iTween.RotateTo(gameObject, iTween.Hash("rotation", new Vector3(0, 270, rot), "time", time, "easetype", iTween.EaseType.easeInOutCubic));
+                }
             }
             rot = rot % 360;
             rotNew = rot < 0 ? rot + 360 : rot;
@@ -86,7 +132,13 @@ public class mainController : MonoBehaviour
                 thudScipt.rotate();
                 StartCoroutine(shiftGrav());
             }
+
         }
+    }
+
+    bool shouldMobileRot(int degree){
+        
+        return Mathf.Min(Mathf.Abs(degree - SAngle),  Mathf.Abs(degree - (SAngle - 360))) < 50;
     }
 
     IEnumerator shiftGrav()
